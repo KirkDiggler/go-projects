4a912902775625b0877919b999f095a50ab37709
Kirk Gilmore <kirklgilmore@gmail.com>
2022-01-04 21:15:32 -0800

    pass new lookup test

--- dynago/mappings/lookup.go
+++ dynago/mappings/lookup.go
@@ -17,7 +17,7 @@ const (
 	requiredLookupFieldsMsg      = "mappings.NewLookup requires LookupConfig.Fields to be set"
 )
 
-type lookupMapping struct {
+type lookup struct {
 	mappingName string
 	fieldMap    map[string]bool
 	fields      []string
@@ -28,7 +28,7 @@ type LookupConfig struct {
 	Fields      []string
 }
 
-func NewLookup(cfg *LookupConfig) (*lookupMapping, error) {
+func NewLookup(cfg *LookupConfig) (Interface, error) {
 	if cfg == nil {
 		return nil, errors.New(requiredLookupConfig)
 	}
@@ -47,52 +47,53 @@ func NewLookup(cfg *LookupConfig) (*lookupMapping, error) {
 		fieldMap[field] = true
 	}
 
-	return &lookupMapping{
+	return &lookup{
 		mappingName: cfg.MappingName,
 		fieldMap:    fieldMap,
 		fields:      cfg.Fields,
 	}, nil
 }
 
-func (m *lookupMapping) buildValues(ctx context.Context, values map[string]types.AttributeValue) (string, error) {
+func (m *lookup) buildValues(ctx context.Context, values map[string]types.AttributeValue) (string, error) {
 	var sb = strings.Builder{}
 	for _, field := range m.fields {
 		if _, ok := values[field]; !ok {
 			return "", fmt.Errorf("required field '%s' was not found in provided values", field)
 		}
 
-		_, err := fmt.Fprintf(&sb, "%s%s%s",
+		_, err := fmt.Fprintf(&sb, "%s%s%s%s",
 			setCasing(field),
 			getFieldSeparator(),
-			setCasing(attributeValueToString(values[field])))
+			setCasing(attributeValueToString(values[field])),
+			getFieldSeparator())
 		if err != nil {
 			return "", fmt.Errorf("error returned when formatting partition field. original error: %s", err)
 		}
 	}
 
-	return sb.String(), nil
+	return sb.String()[:sb.Len()-1], nil
 }
 
-func (m *lookupMapping) BuildPartitionValues(ctx context.Context, values map[string]types.AttributeValue) (string, error) {
+func (m *lookup) BuildPartitionValues(ctx context.Context, values map[string]types.AttributeValue) (string, error) {
 	return m.buildValues(ctx, values)
 }
 
-func (m *lookupMapping) BuildSortValues(ctx context.Context, values map[string]types.AttributeValue) (string, error) {
+func (m *lookup) BuildSortValues(ctx context.Context, values map[string]types.AttributeValue) (string, error) {
 	return m.buildValues(ctx, values)
 }
 
-func (m *lookupMapping) GetName() string {
+func (m *lookup) GetName() string {
 	return m.mappingName
 }
 
-func (m *lookupMapping) GetType() entities.MappingType {
+func (m *lookup) GetType() entities.MappingType {
 	return entities.MappingType_Lookup
 }
 
-func (m *lookupMapping) GetPartitionFields() []string {
+func (m *lookup) GetPartitionFields() []string {
 	return m.fields
 }
 
-func (m *lookupMapping) GetSortFields() []string {
+func (m *lookup) GetSortFields() []string {
 	return m.fields
 }
